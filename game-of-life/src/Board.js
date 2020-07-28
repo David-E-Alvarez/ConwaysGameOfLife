dcdfe30728bdf5019ee6d2928278a959dc4fc4fb
David-E-Alvarez <davidealvarez1@gmail.com>
2020-07-28 15:07:22 -0700

    test edit on hp for yoga laptop

--- game-of-life/src/Board.js
+++ game-of-life/src/Board.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Board = () => {
-    return <h1>Board.js</h1>
+    return <h1>Board.js(testing)</h1>
 }
 
 export default Board
--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import './Board.js'
 import Board from './Board.js'
 
 class App extends Component {
