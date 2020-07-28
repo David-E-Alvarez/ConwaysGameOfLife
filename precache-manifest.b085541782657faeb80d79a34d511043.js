c8eb35f63e6177279577ce811945bcfdf1d6d2e1
David-E-Alvarez <davidealvarez1@gmail.com>
2020-07-27 20:37:16 -0700

    Updates

---
+++ precache-manifest.b085541782657faeb80d79a34d511043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d466d27110d6d8c9a63cab2481466eb9",
+    "url": "/ConwaysGameOfLife/index.html"
+  },
+  {
+    "revision": "03931016bee1c412cf0d",
+    "url": "/ConwaysGameOfLife/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "72108465ac20f49b1255",
+    "url": "/ConwaysGameOfLife/static/js/2.b08c8b92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ConwaysGameOfLife/static/js/2.b08c8b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03931016bee1c412cf0d",
+    "url": "/ConwaysGameOfLife/static/js/main.a6907394.chunk.js"
+  },
+  {
+    "revision": "fc570e04274d8abae78b",
+    "url": "/ConwaysGameOfLife/static/js/runtime-main.b355990a.js"
+  }
+]);
