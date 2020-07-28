d298565c91090625d410a2c19e9651cb56909d30
David-E-Alvarez <davidealvarez1@gmail.com>
2020-07-28 09:44:34 -0700

    added conway game rules

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -6,8 +6,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Future home of Conway's Game of Life</h1>
-        <h2>Did You hear me lad?</h2>
+        <h1>Conway's Game of Life</h1>
+        <section>
+          <h5>Rules:</h5>
+          <ul>
+            <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation</li>
+            <li>Any live cell with two or three live neighbours lives on to the next generation</li>
+            <li>Any live cell with more than three live neighbours dies, as if by overpopulation</li>
+            <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction</li>
+          </ul>
+          <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules">Source</a>
+        </section>
       </div>
     )
   }
