9da6239a8a5a74cbf31e2b1c7666b7753c044999
David-E-Alvarez <davidealvarez1@gmail.com>
2020-07-29 15:27:52 -0700

    Updates

---
+++ precache-manifest.01d3b39d310f1af4aad4bbc0402a60ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401694df75f2ae7e4c5167ecbaed4c7e",
+    "url": "/ConwaysGameOfLife/index.html"
+  },
+  {
+    "revision": "3fa86e080453766b3cff",
+    "url": "/ConwaysGameOfLife/static/css/main.7e43bf92.chunk.css"
+  },
+  {
+    "revision": "72108465ac20f49b1255",
+    "url": "/ConwaysGameOfLife/static/js/2.b08c8b92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ConwaysGameOfLife/static/js/2.b08c8b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa86e080453766b3cff",
+    "url": "/ConwaysGameOfLife/static/js/main.c3c048b7.chunk.js"
+  },
+  {
+    "revision": "fc570e04274d8abae78b",
+    "url": "/ConwaysGameOfLife/static/js/runtime-main.b355990a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ConwaysGameOfLife/precache-manifest.2f8aba35670d9248527f975aa5fcb566.js"
+  "/ConwaysGameOfLife/precache-manifest.01d3b39d310f1af4aad4bbc0402a60ff.js"
 );
 
 self.addEventListener('message', (event) => {
