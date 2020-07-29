77f85d4abe8377b854828ebbd91f762f1cbc1e58
David-E-Alvarez <davidealvarez1@gmail.com>
2020-07-29 15:18:22 -0700

    grid is up!

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -2,18 +2,95 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class App extends Component {
-  constructor(){
-    super();
-    this.rows = 25;
-    this.columns = 25;
-    this.state = {
-      generation: 0,
+//grid dimensions
+const rows = 25;
+const columns = 25;
+
+//initial board
+const new_board = (status_of_cell = () => false) => {//sets every cell on board to false
+  const grid = [];
+  for(var i = 0; i< rows; i++){
+    grid[i] = [];
+    for(var j = 0; j < columns; j++){
+      grid[i][j] = status_of_cell()
     }
   }
+  return grid;
+
+};
+
+//board component
+const Board = ({board_status, toggle_cell}) =>{
+  const handleClick = (i,j) => toggle_cell(i,j);
+  const table_rows = [];
+  for(var i = 0; i < rows; i++){
+    const table_data = [];
+    for(var j = 0; j < columns; j++){
+      table_data.push(
+        <td
+          key={`${i},${j}`}
+          className={board_status[i][j] ? 'alive':'dead'}
+          onClick={()=>handleClick(i,j)}
+        />
+      );
+    }
+    table_rows.push(<tr key={i}>{table_data}</tr>)
+  } 
+  return <table><tbody>{table_rows}</tbody></table>;
+};
 
 
+class App extends Component {
+  //state
+  state = {
+    board_status: new_board(),
+    generation: 0,
+    isGameRunning: false,
+  };
+  //function for start/stop buttons
+  buttons = () => {
+    return this.state.isGameRunning ?
+    <button type='button' onClick={this.handle_stop}>Stop</button>:
+    <button type='button' onClick={this.handle_start}>Start</button>;
+  }
+
+  //clear board
+  clear_board = () => {
+    this.setState({
+      board_status: new_board(()=> false),
+      generation: 0
+    });
+  }
+  //new board
+  new_board_instance = () => {
+    this.setState({
+      board_status: new_board(),
+      generation:0
+    });
+  }
+
+  //handle cell toggling
+  handle_toggle_cell_status = (i,j) => {
+    const toggle_board_status = prev_state => {
+      const board_copy = JSON.parse(JSON.stringify(prev_state.board_status));
+      board_copy[i][j] = !board_copy[i][j];
+      return board_copy;
+    };
+    this.setState(prev_state => ({
+      board_status: toggle_board_status(prev_state)
+    }))
+  }
+
+  //stop/run state
+  handle_start = () => {
+		this.setState({ isGameRunning: true });
+	}
+
+	handle_stop = () => {
+		this.setState({ isGameRunning: false });
+	}
   render() {
+    const {board_status,isGameRunning,generation} = this.state;
     return (
       <div className="App">
         <h1>Conway's Game of Life</h1>
@@ -27,7 +104,7 @@ class App extends Component {
           </ul>
           <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules">Wikipedia</a>
         </section>
-        <h2>Generations: {this.state.generation}</h2>
+        <Board board_status={board_status} toggle_cell={this.handle_toggle_cell_status}/>
       </div>
     )
   }
